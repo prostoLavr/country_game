dff147e6c613a1d13366224f7f05730ad997df5a
Lawrence Naumov <prostolawr@gmail.com>
2022-06-26 13:04:49 +0800

    Edit to english

--- excel_to_map.py
+++ excel_to_map.py
@@ -1,8 +1,8 @@
 from openpyxl import load_workbook
 import os
 import pickle
-read_file = input('Файл экселя: ').strip()
-write_file = os.path.join('maps', input('Имя карты: ').strip() + ".pickle")
+read_file = input('Excel file: ').strip()
+write_file = os.path.join('maps', input('Map name: ').strip() + ".pickle")
 wb = load_workbook(read_file)
 ws = wb.active
 map_list = [[cell.value for cell in row] for row in ws.iter_rows()]
